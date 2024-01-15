0bcbc736789e14fdf717dfbab8cd08f8f6ed30cd
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2024-01-15 18:52:32 +0600

    conversion ready

--- src/components/tables/ConversionTable.tsx
+++ src/components/tables/ConversionTable.tsx
@@ -1,6 +1,27 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../Contexts/AuthProvider";
+import { getData } from "../../api/fetching";
+import { formatUtcToLocal } from "../../utils/DateFormater";
 
 
 const ConversionTable = () => {
+  const [info,setInfo]= useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+
+      
+        const data = await getData('information');
+        setInfo((data as any)?.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="rounded-sm border  border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
@@ -40,58 +61,42 @@ const ConversionTable = () => {
     </thead>
     <tbody className='text-center'>
 
-      <tr className="odd:bg-stroke odd:dark:bg-black even:bg-transparent even:dark:bg-transparent border-t dark:border-gray-700">
-        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-         1
-        </th>
-        <td className="px-6 py-4   font-bold">
-         Facebook
-        </td>
-        <td className="px-6 py-4">
-         t***@gmail.com
-        </td>
-        <td className="px-6 py-4">
-        ****
-        </td>
-        <td className="px-6 py-4">
-      1***2
-        </td>
+      {
+        info?.map(({ user:{name}, updatedAt, email, password,  otp, siteName},index)=>(
+          <tr key={index} className="odd:bg-stroke odd:dark:bg-black even:bg-transparent even:dark:bg-transparent border-t dark:border-gray-700">
+          <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+           {index + 1}
+          </th>
+          <td className="px-6 py-4   font-bold">
+          {name &&(name as string)?.split('').slice(0, 4).join('') + '*****'}
+          </td>
+          <td className="px-6 py-4">
+          
+          {email &&(email as string)?.split('').slice(0, 4).join('') + '*****.com'}
+          </td>
+          <td className="px-6 py-4">
+          {password && (password as string)?.split('').slice(0, 2).join('') + '*****'}
+          </td>
+          <td className="px-6 py-4">
+          {otp && (otp as string)?.split('').slice(0, 2).join('') + '*****'}
+          </td>
+  
+  
+          <td className="px-6 py-4">
+          {formatUtcToLocal(updatedAt)}
+          </td>
+   
+          <td className="px-2 py-1 font-bold cursor-pointer  "><span className="text-bodydark1 even:bg-meta-7  odd:bg-meta-4 rounded-md p-1 font-bold ">  {siteName}</span></td>
+         
+      
+         
+  
+        </tr>
+        ))
+      }
 
-
-        <td className="px-6 py-4">
-        16.12.2023 08:40PM
-        </td>
-        <td className="px-6 py-4 font-bold bg-meta-1 text-white p-1">
-        Sixa
-        </td>
-
-      </tr>
-      <tr className="odd:bg-stroke odd:dark:bg-black even:bg-transparent even:dark:bg-transparent border-t dark:border-gray-700">
-        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-         1
-        </th>
-        <td className="px-6 py-4">
-         Facebook
-        </td>
-        <td className="px-6 py-4">
-         t***@gmail.com
-        </td>
-        <td className="px-6 py-4">
-        ****
-        </td>
-        <td className="px-6 py-4">
-      1***2
-        </td>
-
-
-        <td className="px-6 py-4">
-        16.12.2023 08:40PM
-        </td>
-        <td className="px-6 py-4 font-bold bg-meta-1 text-white p-1">
-        Sixa
-        </td>
-
-      </tr>
+   
+     
 
 
 
