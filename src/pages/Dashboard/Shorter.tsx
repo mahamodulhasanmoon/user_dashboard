7695408b673dae8f1f3e93ab802680d8f1889e01
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2024-01-29 14:58:37 +0600

    free shortner added

--- src/pages/Dashboard/Shorter.tsx
+++ src/pages/Dashboard/Shorter.tsx
@@ -4,17 +4,38 @@ import { getData, postData } from '../../api/fetching';
 import { handleCopyClick } from '../../utils/copyToClipboard';
 import toast from 'react-hot-toast';
 import CompLoader from '../../common/Loader/CompLoader';
+import axios from 'axios';
 
 export default function Shorter() {
   const [fullUrl, setfullUrls] = useState('');
-  const [urls, setUrls] = useState([]);
+  const [urls, setUrls] = useState<any>([]);
   const [loading, setLoading] = useState(false);
 
   const handleShortUrl = async () => {
     try {
-      await postData(`shortlinks`, {
-        fullUrl: fullUrl,
-      });
+      // await postData(`shortlinks`, {
+      //   fullUrl: fullUrl,
+      // });
+
+
+      const encodedParams = new URLSearchParams();
+encodedParams.set('url', fullUrl);
+
+const options = {
+method: 'POST',
+url: 'https://spoo.me/',
+headers: {
+Accept: 'application/json'
+},
+data: encodedParams,
+};
+
+const response = await axios.request(options);
+
+setUrls((prevUrls:any) => [...prevUrls, response?.data?.short_url])
+console.log(response.data);
+
+    
       toast.success('successfull');
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -35,7 +56,7 @@ export default function Shorter() {
     };
 
     fetchData();
-  }, [fullUrl]);
+  }, []);
 
   return (
     <div>
@@ -46,9 +67,9 @@ export default function Shorter() {
         <div className="flex flex-col gap-5.5 p-6.5">
           <div className="flex">
           <select className='rounded-tl-lg rounded-bl-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary' name="" id="">
-            <option value="select One">select One</option>
-            <option value="select One">mega.xyz</option>
-            <option value="select One">megs.xyz</option>
+           
+            <option value="https://spoo.me" selected>https://spoo.me</option>
+            
           </select>
             <input
               type="text"
@@ -74,7 +95,7 @@ export default function Shorter() {
         </h4>
 
         <div className="flex flex-col">
-          <div className="grid grid-cols-4 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-4">
+          {/* <div className="grid grid-cols-4 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-4">
             <div className="p-2.5 xl:p-5">
               <h5 className="text-sm font-medium uppercase xsm:text-base">
                 Website
@@ -96,13 +117,33 @@ export default function Shorter() {
                 Action
               </h5>
             </div>
+          </div> */}
+
+<div className="grid grid-cols-2 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-2">
+            {/* <div className="p-2.5 xl:p-5">
+              <h5 className="text-sm font-medium uppercase xsm:text-base">
+                Website
+              </h5>
+            </div> */}
+            <div className="p-2.5 text-center xl:p-5">
+              <h5 className="text-sm font-medium uppercase xsm:text-base">
+                Short URL
+              </h5>
+            </div>
+
+            <div className="hidden p-2.5 text-center sm:block xl:p-5">
+              <h5 className="text-sm font-medium uppercase xsm:text-base">
+                Action
+              </h5>
+            </div>
           </div>
+
           {loading && (
             <div>
               <CompLoader />
             </div>
           )}
-          {urls?.map(({ fullUrl, shortUrl, clicks }, index) => (
+          {/* {urls?.map(({ fullUrl, shortUrl, clicks }, index) => (
             <div key={index} className="grid grid-cols-4 sm:grid-cols-4">
               <div className="flex items-center gap-3 p-2.5 xl:p-5">
                 <Link
@@ -127,9 +168,36 @@ export default function Shorter() {
               </div>
 
               <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-                {/* button */}
+               
                 <button
                   onClick={() => handleCopyClick(`http://bit.ly/${shortUrl}`)}
+                  className="inline-flex rounded items-center justify-center bg-primary py-3 px-5 text-center font-medium text-white hover:bg-opacity-90 lg:px-5 xl:px-10"
+                >
+                  Copy
+                </button>
+              </div>
+            </div>
+          ))} */}
+
+{urls?.map((val:any) => (
+            <div key={val} className="grid grid-cols-2 sm:grid-cols-2">
+             
+
+              <div className="flex items-center justify-center p-2.5 xl:p-5">
+                <Link
+                  to={val}
+                  className="text-black dark:text-white"
+                >
+                  {val}
+                </Link>
+              </div>
+
+              
+
+              <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
+                {/* button */}
+                <button
+                  onClick={() => handleCopyClick(val)}
                   className="inline-flex rounded items-center justify-center bg-primary py-3 px-5 text-center font-medium text-white hover:bg-opacity-90 lg:px-5 xl:px-10"
                 >
                   Copy
@@ -142,3 +210,7 @@ export default function Shorter() {
     </div>
   );
 }
+
+
+
+
